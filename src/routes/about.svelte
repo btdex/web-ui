1e2727a9c967fd618783c41950feac4c1274c1c6
ohager <oliver.hager@dextra-sw.com>
2020-07-26 20:55:52 -0300

    feat: changed to rollup

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,7 +1,25 @@
-<svelte:head>
-	<title>About</title>
-</svelte:head>
+<div class="container">
+	<Button on:click={() => clicked++}>
+		<Icon class="material-icons">thumb_up</Icon>
+		<Label>Click Me</Label>
+	</Button>
+	<p class="mdc-typography--body1">
+		{#if clicked}
+			You've clicked the button {clicked} time{clicked === 1 ? '' : 's'}.
+		{:else}
+			<span class="grayed">You haven't clicked the button.</span>
+		{/if}
+	</p>
+</div>
 
-<h1>About this site</h1>
+<script>
+	import Button, {Label, Icon} from '@smui/button';
 
-<p>This is the 'about' page. There's not much here.</p>
+	let clicked = 0;
+</script>
+
+<style>
+	.grayed {
+		opacity: .6;
+	}
+</style>
